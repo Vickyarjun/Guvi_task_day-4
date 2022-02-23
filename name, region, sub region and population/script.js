5f75977f36f62a2b6b3deeaca2ec8963717b3ec2
Vignesh <92911455+Vickyarjun@users.noreply.github.com>
2022-02-23 13:54:52 -0500

    Update script.js

--- name, region, sub region and population/script.js
+++ name, region, sub region and population/script.js
@@ -1,29 +1,3 @@
-// var request=new XMLHttpRequest();
-// request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
-// request.send()
-
-// request.onload=function()
-// { if (request.status==200)
-//     {
-//     let a=JSON.parse(request.responseText)
-//     a.map((b)=>{
-//          console.log(b.name.official)});
-//     a.map((c)=>{
-//         console.log(c.region)
-//     });
-//     a.map((d)=>{
-//         console.log(d.subregion)
-//     });
-//     a.map((e)=>{
-//         console.log(e.population)
-//     });
-// }else{
-//     request.onerror=function(){
-//         console.log(error)
-//     }
-// }
-//}
-
 var request=new XMLHttpRequest();
 request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
 request.send()
